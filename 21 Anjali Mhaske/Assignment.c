e8bafda8ce4dc294eab07fda21016ecba7aa396d
anjali-ux-hue <anjalimhaske@eng.rizvi.edu.in>
2024-12-05 08:49:16 +0530

    Assignment.c

---
+++ 21 Anjali Mhaske/Assignment.c
@@ -0,0 +1,29 @@
+/* 
+Name :- Anjali Mhaske 
+Roll No :- 21
+UIN :- 251S026
+Title :- WAP to calculate sum of two matrix
+
+Given by :- Prachi Gawade 
+Roll No :- 21
+UIN :- 241S076
+*/
+#include<stdio.h>
+int main(){
+int num1,num2,sum;
+
+//input two mu,bers from the user
+printf("Enter the first number: ");
+scanf("%d", &num1);
+printf("Enter the second number: ");
+scanf("%d", &num2);
+
+//Calculate the sum
+sum=num1+num2;
+
+//output the result
+printf("The sum of %d and %d is: %d\n", num1,num2,sum);
+
+return 0;
+
+}
