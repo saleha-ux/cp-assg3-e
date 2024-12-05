66e71f6d6a0fc239bb0f9bb5c5ba87ae6b65197c
alzaid00 <alzaidkhan00@eng.rizvi.edu.in>
2024-12-05 13:53:35 +0530

    Create Assignment 3.c

---
+++ 09_Alzaid Khan/Assignment 3.c
@@ -0,0 +1,34 @@
+/*
+
+Assignment no. 3
+
+Title : calculation of square
+
+Name: Mohd Alzaid Khan
+
+Class & Div: ECS-E
+
+UIN: 241S009
+
+Assignment given by : ASHFAQUE SAYYED
+UIN : 241S005
+Roll.no : 05
+
+*/
+
+#include <stdio.h>
+
+
+int main() {
+    int a;
+
+    // User input
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    // Calculating the square 
+     printf(" Square of %d is %d",a,a*a);
+
+
+    return 0;
+}
